7119ae296f554f28aea54acc625cd73a77f5ce82
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-16 18:20:51 +0330

    config two-steps page

--- src/app/auth/authForms/AuthTwoSteps.tsx
+++ src/app/auth/authForms/AuthTwoSteps.tsx
@@ -5,50 +5,50 @@ import CustomFormLabel from "@/app/(DashboardLayout)/components/forms/theme-elem
 import { Stack } from "@mui/system";
 
 const AuthTwoSteps = () => (
-  <>
-    <Box mt={4}>
-      <Stack mb={3}>
-        <CustomFormLabel htmlFor="code">
-          Type your 6 digits security code{" "}
-        </CustomFormLabel>
-        <Stack spacing={2} direction="row">
-          <CustomTextField id="code" variant="outlined" fullWidth />
-          <CustomTextField id="code" variant="outlined" fullWidth />
-          <CustomTextField id="code" variant="outlined" fullWidth />
-          <CustomTextField id="code" variant="outlined" fullWidth />
-          <CustomTextField id="code" variant="outlined" fullWidth />
-          <CustomTextField id="code" variant="outlined" fullWidth />
-        </Stack>
-      </Stack>
-      <Button
-        color="primary"
-        variant="contained"
-        size="large"
-        fullWidth
-        component={Link}
-        href="/"
-      >
-        Verify My Account
-      </Button>
+    <>
+        <Box mt={4}>
+            <Stack mb={3}>
+                <CustomFormLabel htmlFor="code">
+                    کد 6 رقمی را وارد کنید{" "}
+                </CustomFormLabel>
+                <Stack spacing={2} direction="row">
+                    <CustomTextField id="code" variant="outlined" fullWidth />
+                    <CustomTextField id="code" variant="outlined" fullWidth />
+                    <CustomTextField id="code" variant="outlined" fullWidth />
+                    <CustomTextField id="code" variant="outlined" fullWidth />
+                    <CustomTextField id="code" variant="outlined" fullWidth />
+                    <CustomTextField id="code" variant="outlined" fullWidth />
+                </Stack>
+            </Stack>
+            <Button
+                color="primary"
+                variant="contained"
+                size="large"
+                fullWidth
+                component={Link}
+                href="/"
+            >
+                احراز هویت
+            </Button>
 
-      <Stack direction="row" spacing={1} mt={3}>
-        <Typography color="textSecondary" variant="h6" fontWeight="400">
-          Didn&apos;t get the code?
-        </Typography>
-        <Typography
-          component={Link}
-          href="/"
-          fontWeight="500"
-          sx={{
-            textDecoration: "none",
-            color: "primary.main",
-          }}
-        >
-          Resend
-        </Typography>
-      </Stack>
-    </Box>
-  </>
+            <Stack direction="row" spacing={1} mt={3} justifyContent='center'>
+                <Typography color="textSecondary" variant="h6" fontWeight="400">
+                    کد را دریافت نکردید ؟
+                </Typography>
+                <Typography
+                    component={Link}
+                    href="/"
+                    fontWeight="500"
+                    sx={{
+                        textDecoration: "none",
+                        color: "primary.main",
+                    }}
+                >
+                    دوباره ارسال کن
+                </Typography>
+            </Stack>
+        </Box>
+    </>
 );
 
 export default AuthTwoSteps;
