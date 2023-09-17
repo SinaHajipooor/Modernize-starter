0621465bdd531ebb4ea008c29fc42ed95fdf9663
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-17 09:24:20 +0330

    define post requests for user register

--- src/app/(DashboardLayout)/loading.tsx
+++ src/app/(DashboardLayout)/loading.tsx
@@ -5,17 +5,6 @@ import { LinearProgress } from "@mui/material";
 
 export default function Loading() {
     return (
-        //     <Box
-        //       sx={{
-        //         display: "flex",
-        //         justifyContent: "center",
-        //         alignItems: "center",
-        //         width: "100%",
-        //         height: "100vh",
-        //       }}
-        //     >
-        //       <CircularProgress />
-        //     </Box>
 
         <Box sx={{ width: '100%' }}>
             <LinearProgress variant="determinate" value={50} />
