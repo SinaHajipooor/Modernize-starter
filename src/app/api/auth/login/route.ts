a860939020b66be21d60669ae504990c97fa43e1
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-18 18:35:17 +0330

    get api for token

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// set cookie 
 export async function POST(request: NextRequest) {
     const reqBody = await request.json();
     // extract user token from request body 
@@ -11,7 +12,18 @@ export async function POST(request: NextRequest) {
     })
     // set token in cookie
     response.cookies.set('token', token, { httpOnly: true })
+
     return response;
 }
 
 
+// get cookie
+export async function GET() {
+    const response = NextResponse.json({
+        message: 'token has been get from cookies',
+        success: true,
+    })
+    //  get token from cookies
+    const token = response.cookies.get('token');
+    return token;
+}
