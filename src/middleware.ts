6e4da48ef131be69e20f9076e081d1ec7698385b
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-19 12:12:59 +0330

    protect routes with middleware

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse, NextRequest } from "next/server";
+
+
+// loging part
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname;
+    const isPublicPath = path === '/auth/login' || path === '/auth/register' || path === '/auth/two-steps' || path === '/auth/forgot-password';
+
+    // Extract token from cookies
+    const token = request.cookies.get('token')?.value || '';
+    // Redirect the authenticated users into dashboard
+    if (isPublicPath && token) {
+        console.log('there is a token');
+        return NextResponse.redirect(new URL('/', request.nextUrl));
+    }
+
+    // Redirect the users that are not authenticated into the login page
+    if (!isPublicPath && !token) {
+        console.log('not token');
+        return NextResponse.redirect(new URL('/auth/login', request.nextUrl));
+    }
+}
+
+
+// matchin part 
+export const config = {
+    matcher: ['/', '/auth/login', '/auth/register', '/auth/two-steps', '/auth/forgot-password']
+}
