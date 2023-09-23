d83731818da3a888d4a6f0cee895c8b01a2dbd66
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-23 08:57:33 +0330

    config dashboard loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,20 +1,22 @@
 'use client'
+import { LinearProgress } from "@mui/material";
 import Box from "@mui/material/Box";
 import { SyncLoader } from "react-spinners";
 
 export default function Loading() {
     return (
-        <Box
-            sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-                height: "100vh",
-            }}
-        >
-            <SyncLoader color="#36afd7" />
+        // <Box
+        //     sx={{
+        //         display: "flex",
+        //         justifyContent: "center",
+        //         alignItems: "center",
+        //         width: "100%",
+        //         height: "100vh",
+        //     }}
+        // >
+        //     {/* <SyncLoader color="#36afd7" /> */}
 
-        </Box>
+        // </Box>
+        <LinearProgress />
     );
 };
