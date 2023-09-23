1405794ba495ef128d48b1e8a56eff58f81155ca
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-23 21:37:06 +0330

    optimize remote api

--- src/utils/api/activity-histories/apiActivityHistories.ts
+++ src/utils/api/activity-histories/apiActivityHistories.ts
@@ -8,10 +8,8 @@ export async function apiFetchAllActivityHistories() {
     try {
         const response = await axiosConfig.get(`${ACTIVITY_BASE_URL}?user_id=1`);
         const data = response.data.result.data;
-        console.log(data);
         return data;
     } catch (error: any) {
-        console.log(error.message)
         throw new Error(error.message)
     }
 }
