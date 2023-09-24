3b6546838cb82801e73506e32fc83d99fd4a6a41
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-24 21:26:23 +0330

    add bread crumb

--- src/app/(DashboardLayout)/activity-histories/index/page.tsx
+++ src/app/(DashboardLayout)/activity-histories/index/page.tsx
@@ -7,8 +7,18 @@ import Table2 from '@/app/(DashboardLayout)/components/tables/Table2';
 import Link from 'next/link';
 import Spinner from '../../components/ui/Spinner';
 import useActivityIndex from '../hooks/useActivityIndex';
+import Breadcrumb from '@/app/(DashboardLayout)/layout/shared/breadcrumb/Breadcrumb';
 
 
+const BCrumb = [
+    {
+        to: '/',
+        title: 'داشبورد',
+    },
+    {
+        title: 'سوابق فعالیت',
+    },
+];
 
 
 const BasicTable = () => {
@@ -18,28 +28,29 @@ const BasicTable = () => {
     return (
         <Box mt={2}>
             <PageContainer title="Basic Table" description="this is Basic Table">
-                <ParentCard title="فهرست سوابق فعالیت" >
-                    {isLoading ? <Box mb={5}><Spinner /> </Box> : <>
-                        <Button
-                            color="primary"
-                            variant="contained"
-                            size="large"
-                            href="/activity-histories/create"
-                            LinkComponent={Link}
-                        >
-                            ایجاد
-                        </Button>
-                        <Grid container spacing={3}>
-                            <Grid item xs={12}>
-                            </Grid>
-                            <Grid item xs={12} >
-                                <Box>
-                                    <Table2 data={activityHistories} />
-                                </Box>
-                            </Grid>
+                <Breadcrumb title="سوابق فعالیت" items={BCrumb} />
+                {/* <ParentCard title="فهرست سوابق فعالیت" > */}
+                {isLoading ? <Box mb={5}><Spinner /> </Box> : <>
+                    <Button
+                        color="primary"
+                        variant="contained"
+                        size="large"
+                        href="/activity-histories/create"
+                        LinkComponent={Link}
+                    >
+                        ایجاد
+                    </Button>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
                         </Grid>
-                    </>}
-                </ParentCard>
+                        <Grid item xs={12} >
+                            <Box>
+                                <Table2 data={activityHistories} />
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </>}
+                {/* </ParentCard> */}
             </PageContainer>
         </Box>
     );
