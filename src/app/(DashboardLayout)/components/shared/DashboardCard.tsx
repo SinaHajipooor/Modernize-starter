109c663a9581a3ea9bd5ab8c36c42046056b054f
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-26 11:08:18 +0330

    sty;e dashboard sale chart

--- src/app/(DashboardLayout)/components/shared/DashboardCard.tsx
+++ src/app/(DashboardLayout)/components/shared/DashboardCard.tsx
@@ -4,79 +4,79 @@ import { useSelector } from '@/store/hooks';
 import { AppState } from '@/store/store';
 
 type Props = {
-  title?: string;
-  subtitle?: string;
-  action?: JSX.Element | any;
-  footer?: JSX.Element;
-  cardheading?: string | JSX.Element;
-  headtitle?: string | JSX.Element;
-  headsubtitle?: string | JSX.Element;
-  children?: JSX.Element;
-  middlecontent?: string | JSX.Element;
+    title?: string;
+    subtitle?: string;
+    action?: JSX.Element | any;
+    footer?: JSX.Element;
+    cardheading?: string | JSX.Element;
+    headtitle?: string | JSX.Element;
+    headsubtitle?: string | JSX.Element;
+    children?: JSX.Element;
+    middlecontent?: string | JSX.Element;
 };
 
 const DashboardCard = ({
-  title,
-  subtitle,
-  children,
-  action,
-  footer,
-  cardheading,
-  headtitle,
-  headsubtitle,
-  middlecontent,
+    title,
+    subtitle,
+    children,
+    action,
+    footer,
+    cardheading,
+    headtitle,
+    headsubtitle,
+    middlecontent,
 }: Props) => {
-  const customizer = useSelector((state: AppState) => state.customizer);
+    const customizer = useSelector((state: AppState) => state.customizer);
 
-  const theme = useTheme();
-  const borderColor = theme.palette.divider;
+    const theme = useTheme();
+    const borderColor = theme.palette.divider;
 
-  return (
-    <Card
-      sx={{ padding: 0, border: !customizer.isCardShadow ?  `1px solid ${borderColor}` : 'none' }}
-      elevation={customizer.isCardShadow ? 9 : 0}
-      variant={!customizer.isCardShadow ? 'outlined' : undefined}
-    >
-      {cardheading ? (
-        <CardContent>
-          <Typography variant="h5">{headtitle}</Typography>
-          <Typography variant="subtitle2" color="textSecondary">
-            {headsubtitle}
-          </Typography>
-        </CardContent>
-      ) : (
-        <CardContent sx={{p: "30px"}}>
-          {title ? (
-            <Stack
-              direction="row"
-              spacing={2}
-              justifyContent="space-between"
-              alignItems={'center'}
-              mb={3}
-            >
-              <Box>
-                {title ? <Typography variant="h5">{title}</Typography> : ''}
+    return (
+        <Card
+            sx={{ padding: 0, border: !customizer.isCardShadow ? `1px solid ${borderColor}` : 'none', boxShadow: '0px 3px 8px rgba(0, 0, 0, 0.1)' }}
+            elevation={customizer.isCardShadow ? 9 : 0}
+            variant={!customizer.isCardShadow ? 'outlined' : undefined}
+        >
+            {cardheading ? (
+                <CardContent>
+                    <Typography variant="h5">{headtitle}</Typography>
+                    <Typography variant="subtitle2" color="textSecondary">
+                        {headsubtitle}
+                    </Typography>
+                </CardContent>
+            ) : (
+                <CardContent sx={{ p: "30px" }}>
+                    {title ? (
+                        <Stack
+                            direction="row"
+                            spacing={2}
+                            justifyContent="space-between"
+                            alignItems={'center'}
+                            mb={3}
+                        >
+                            <Box>
+                                {title ? <Typography variant="h5">{title}</Typography> : ''}
 
-                {subtitle ? (
-                  <Typography variant="subtitle2" color="textSecondary">
-                    {subtitle}
-                  </Typography>
-                ) : (
-                  ''
-                )}
-              </Box>
-              {action}
-            </Stack>
-          ) : null}
+                                {subtitle ? (
+                                    <Typography variant="subtitle2" color="textSecondary">
+                                        {subtitle}
+                                    </Typography>
+                                ) : (
+                                    ''
+                                )}
+                            </Box>
+                            {action}
+                        </Stack>
+                    ) : null}
 
-          {children}
-        </CardContent>
-      )}
+                    {children}
+                </CardContent>
+            )}
 
-      {middlecontent}
-      {footer}
-    </Card>
-  );
+            {middlecontent}
+            {footer}
+        </Card>
+    );
 };
 
 export default DashboardCard;
