0784b4c1a97b2407c036dbea6bbf68ebf4d43179
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-27 21:53:45 +0330

    optimize root loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,22 +1,10 @@
 'use client'
 import { LinearProgress } from "@mui/material";
 import Box from "@mui/material/Box";
-import { SyncLoader } from "react-spinners";
 
 export default function Loading() {
     return (
-        // <Box
-        //     sx={{
-        //         display: "flex",
-        //         justifyContent: "center",
-        //         alignItems: "center",
-        //         width: "100%",
-        //         height: "100vh",
-        //     }}
-        // >
-        //     {/* <SyncLoader color="#36afd7" /> */}
 
-        // </Box>
         <LinearProgress />
     );
 };
