3aeb24e87ddea2c0328d15d0e01e9af7aa6d1e92
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-09-30 19:16:01 +0330

    add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import { useSelector } from "@/store/hooks";
 import { AppState } from "@/store/store";
 import { Provider } from "react-redux";
 import NextTopLoader from 'nextjs-toploader';
-
+import './global.css'
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
 import "@/app/api/index";
@@ -19,58 +19,58 @@ import Head from "next/head";
 
 
 export const MyApp = ({ children }: { children: React.ReactNode }) => {
-  const theme = ThemeSettings();
+    const theme = ThemeSettings();
 
-  const customizer = useSelector((state: AppState) => state.customizer);
+    const customizer = useSelector((state: AppState) => state.customizer);
 
-  return (
-    <>
-    
-      <NextTopLoader color="#5D87FF" />
-      <NextAppDirEmotionCacheProvider options={{ key: 'modernize' }}>
-      <ThemeProvider theme={theme}>
-        <RTL direction={customizer.activeDir}>
-          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-          <CssBaseline />
-          {children}
-        </RTL>
-      </ThemeProvider>
-      </NextAppDirEmotionCacheProvider>
-    </>
-  );
+    return (
+        <>
+
+            <NextTopLoader color="#5D87FF" />
+            <NextAppDirEmotionCacheProvider options={{ key: 'modernize' }}>
+                <ThemeProvider theme={theme}>
+                    <RTL direction={customizer.activeDir}>
+                        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                        <CssBaseline />
+                        {children}
+                    </RTL>
+                </ThemeProvider>
+            </NextAppDirEmotionCacheProvider>
+        </>
+    );
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  const [loading, setLoading] = React.useState(false);
-  React.useEffect(() => {
-    setTimeout(() => setLoading(true), 3000);
-  }, []);
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body>
-        <Provider store={store}>
-          {loading ? (
-            // eslint-disable-next-line react/no-children-prop
-            <MyApp children={children} />
-          ) : (
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-                height: "100vh",
-              }}
-            >
-              <CircularProgress />
-            </Box>
-          )}
-        </Provider>
-      </body>
-    </html>
-  );
+    const [loading, setLoading] = React.useState(false);
+    React.useEffect(() => {
+        setTimeout(() => setLoading(true), 3000);
+    }, []);
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body>
+                <Provider store={store}>
+                    {loading ? (
+                        // eslint-disable-next-line react/no-children-prop
+                        <MyApp children={children} />
+                    ) : (
+                        <Box
+                            sx={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                width: "100%",
+                                height: "100vh",
+                            }}
+                        >
+                            <CircularProgress />
+                        </Box>
+                    )}
+                </Provider>
+            </body>
+        </html>
+    );
 }
